4869849f62025724fb03786b5d5d011713536d46
David Boedigheimer <stealth259@gmail.com>
2019-10-22 11:43:09 -0700

    Add backend varable with new array created.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,20 @@
 // Back end
-
+var wordPlay = function(oldArray) {
+  var newArray = [];
+  oldArray.forEach(function(word) {
+    if (word.length >= 3) {
+      newArray.push(word);
+    };
+  });
+  return newArray.reverse().join(" ");
+};
 // Front end
 $(document).ready(function() {
   $("form#sentence").submit(function(e) {
-    var set1 = [];
     var origSent = $("#words").val().split(" ");
     console.log(origSent);
-    // $("#show-sent").show();
+    $("#show-sent").prepend(wordPlay(origSent));
+
 
     e.preventDefault();
   });
